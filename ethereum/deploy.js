d1b90baf161830d353759f8bb745ab65bd9be5b5
Seth McCullough <seth@endevrs.dev>
2021-09-23 13:28:30 -0500

    Deploy contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+const interface = compiledFactory.abi;
+const bytecode = compiledFactory.evm.bytecode.object;
+
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: process.env.MNEMONIC,
+  },
+  providerOrUrl: process.env.RINKEBY_API,
+});
+
+const web3 = new Web3(provider);
+
+(async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("Attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(interface)
+      .deploy({ data: bytecode })
+      .send({ from: accounts[0], gas: "5000000" });
+
+    console.log("Contract deployed to", result.options.address);
+
+    provider.engine.stop();
+  } catch (error) {
+    console.error(error);
+  }
+})();
